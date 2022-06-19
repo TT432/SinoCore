717e791e34402476460a7dbd1137c3826de61072
luqin2007 <lq2007lq@hotmail.com>
2022-06-19 17:49:11 +0800

    feat: more shapeless checkers

--- src/api/java/games/moegirl/sinocraft/sinocore/api/crafting/CraftHelper.java
+++ src/api/java/games/moegirl/sinocraft/sinocore/api/crafting/CraftHelper.java
@@ -3,44 +3,48 @@ package games.moegirl.sinocraft.sinocore.api.crafting;
 import it.unimi.dsi.fastutil.Pair;
 import it.unimi.dsi.fastutil.ints.*;
 import net.minecraft.world.Container;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.Ingredient;
 
 import javax.annotation.Nullable;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
+import java.util.function.BiPredicate;
 
 public class CraftHelper {
 
     @Nullable
     public static Int2ObjectMap<Ingredient> matchShapeless(Container inputs, Ingredient[] ingredients) {
-        return matchShapeless(inputs, Arrays.asList(ingredients));
+        return matchShapeless(inputs, ingredients, Ingredient::test);
     }
 
     @Nullable
-    public static Int2ObjectMap<Ingredient> matchShapeless(Container inputs, Iterable<Ingredient> ingredients) {
-        List<Ingredient> notFound = new LinkedList<>();
-        for (Ingredient ingredient : ingredients) {
-            if (!ingredient.isEmpty()) {
-                notFound.add(ingredient);
-            }
-        }
+    public static Int2ObjectMap<Ingredient> matchShapeless(Container inputs, Collection<Ingredient> ingredients) {
+        return matchShapeless(inputs, ingredients, Ingredient::test);
+    }
+
+    @Nullable
+    public static <T> Int2ObjectMap<T> matchShapeless(Container inputs, T[] ingredients, BiPredicate<T, ItemStack> checker) {
+        return matchShapeless(inputs, Arrays.asList(ingredients), checker);
+    }
+
+    @Nullable
+    public static <T> Int2ObjectMap<T> matchShapeless(Container inputs, Collection<T> ingredients, BiPredicate<T, ItemStack> checker) {
+        List<T> notFound = new LinkedList<>(ingredients);
         if (notFound.size() > inputs.getContainerSize()) {
             return null;
         }
-        Int2ObjectMap<Ingredient> result = new Int2ObjectArrayMap<>(notFound.size());
+        Int2ObjectMap<T> result = new Int2ObjectArrayMap<>(notFound.size());
         // loop0
         boolean[] take = new boolean[inputs.getContainerSize()];
         Arrays.fill(take, false);
-        Iterator<Ingredient> iterator = notFound.iterator();
+        Iterator<T> iterator = notFound.iterator();
         while (iterator.hasNext()) {
-            Ingredient ingredient = iterator.next();
+            T ingredient = iterator.next();
             for (int i = 0; i < take.length; i++) {
                 if (take[i]) {
                     continue;
                 }
-                if (ingredient.test(inputs.getItem(i))) {
+                if (checker.test(ingredient, inputs.getItem(i))) {
                     take[i] = true;
                     result.put(i, ingredient);
                     iterator.remove();
@@ -52,14 +56,14 @@ public class CraftHelper {
         }
 
         // loop1: build table
-        List<Pair<Ingredient, IntList>> list = new LinkedList<>();
-        for (Ingredient ingredient : notFound) {
+        List<Pair<T, IntList>> list = new LinkedList<>();
+        for (T ingredient : notFound) {
             IntList l = new IntArrayList();
             for (int i = 0; i < take.length; i++) {
                 if (!take[i]) {
                     continue;
                 }
-                if (ingredient.test(inputs.getItem(i))) {
+                if (checker.test(ingredient, inputs.getItem(i))) {
                     l.add(i);
                 }
             }
@@ -70,18 +74,18 @@ public class CraftHelper {
         }
 
         // loop2: adjust
-        for (Pair<Ingredient, IntList> pair : list) {
+        for (Pair<T, IntList> pair : list) {
             IntList second = pair.second();
             IntListIterator itr = second.iterator();
             while (itr.hasNext()) {
                 int i = itr.nextInt();
-                Ingredient exist = result.get(i);
+                T exist = result.get(i);
                 boolean failed = true;
                 for (int t = 0; t < take.length; t++) {
                     if (take[t]) {
                         continue;
                     }
-                    if (exist.test(inputs.getItem(t))) {
+                    if (checker.test(exist, inputs.getItem(t))) {
                         take[t] = true;
                         result.put(t, exist);
                         result.put(i, pair.first());
